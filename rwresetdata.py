01ebe4b79fc91cec17adef1ef9167542b8222447
Lavanya Jain <lavanyajain2006@gmail.com>
2026-06-21 13:32:41 +0530

    Update rwresetdata.py

--- rwresetdata.py
+++ rwresetdata.py
@@ -1,10 +1,9 @@
 #random walk in 1d with stochastic resetting
-def rw1dreset(n, reset_r):
+def rw1dreset(n, reset_r, n_simulations):
 
     import numpy as np
     import random
-
-    n_simulations = 1000
+    
     position = np.zeros((n_simulations, n))
     for i in range(n_simulations):
         for j in range (1, n):
@@ -31,12 +30,11 @@ def rw1dreset(n, reset_r):
     )
 
 #random walk in 2d with stochastic resetting
-def rw2dreset(n, reset_r):
+def rw2dreset(n, reset_r, n_simulations):
 
     import numpy as np
     import random
-
-    n_simulations = 1000
+    
     x = np.zeros((n_simulations, n))
     y = np.zeros((n_simulations, n))
     for i in range(n_simulations):
@@ -73,11 +71,10 @@ def rw2dreset(n, reset_r):
     )
 
 #random walk in 3d with stochastic resetting
-def rw3dreset(n, reset_r):
+def rw3dreset(n, reset_r, n_simulations):
     import numpy as np
     import random
 
-    n_simulations = 1000
     x = np.zeros((n_simulations, n))
     y = np.zeros((n_simulations, n))
     z = np.zeros((n_simulations, n))
