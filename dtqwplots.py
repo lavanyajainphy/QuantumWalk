6f47e608524c7a88dda70bb7fae0807e4b6e9151
Lavanya Jain <lavanyajain2006@gmail.com>
2026-06-21 13:54:28 +0530

    Update dtqwplots.py

--- dtqwplots.py
+++ dtqwplots.py
@@ -73,8 +73,8 @@ def qw2d(n, coin, initState):
 
         prob = np.sum(np.abs(psi)**2, axis = 2)
 
-        Px = prob.sum(axis=1)
-        Py = prob.sum(axis=0)
+        Px = prob.sum(axis=0)
+        Py = prob.sum(axis=1)
 
         mean_x = np.sum(x * Px)
         mean_y = np.sum(y * Py)
@@ -84,7 +84,7 @@ def qw2d(n, coin, initState):
         msd_list.append(msd)
 
     extent = [-n, n, -n, n]
-    plt.imshow(prob.T, origin="lower", extent=extent)
+    plt.imshow(prob, origin="lower", extent=extent)
     plt.colorbar(label='Probability')
     plt.xlabel("x")
     plt.ylabel("y")
