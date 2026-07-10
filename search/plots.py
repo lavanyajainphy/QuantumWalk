b3fd458d018a056779baed750c589e5e8e7901fb
Lavanya Jain <lavanyajain2006@gmail.com>
2026-07-10 15:09:50 +0530

    Update plots.py

--- search/plots.py
+++ search/plots.py
@@ -6,8 +6,9 @@ def plot_pdet(files, labels, title):
     for file, label in zip(files, labels):
         pdet = np.load(file)["pdet"]
         n = np.load(file)["n"]
-        steps = np.arange(1, n + 1)
-        plt.plot(steps, pdet, label=f"{label}")
+        tau = np.load(file)["tau"]
+        time_steps = np.arange(tau, n + 1, tau)
+        plt.plot(time_steps, pdet, label=f"{label}")
 
     plt.xlabel("Time step")
     plt.ylabel("Cumulative detection probability")
